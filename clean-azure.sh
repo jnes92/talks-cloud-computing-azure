498ff78a27f9a3bec7ea5225406dcc585b828c74
Jonas Österle <jonas.oesterle@brickmakers.de>
2020-04-28 10:08:07 +0200

    Changed account to mpn 85 abo

--- clean-azure.sh
+++ clean-azure.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-az account set -s 070bbfdf-7f1e-4ed8-89bc-8f025a1e9cfa
-groups=$(az group list --query "[?!contains(name, 'vt') && !contains(name, 'cloud') ].name" -o tsv)
+az account set -s e227d55e-6048-4b52-bcbc-123588ad6373
+groups=$(az group list --query "[?!contains(name, 'bm') ].name" -o tsv)
 
 for name in $groups 
 do 
     echo Going to delete: $name
-	az group delete -n $name -y --no-wait
+	az group delete -n $name -y
     echo Deleted.
 done
